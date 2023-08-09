8df190fafbfe72c00a6874b25547e49a56df4068
xrishdog <108433356+xrishdog@users.noreply.github.com>
2023-08-09 15:45:19 -0400

    Rounded to one decimal in tempConversion using toFixed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,6 +2,7 @@ const leapYears = function(year) {
 	if (year%400 == 0 || (year % 4==0 && year%100 != 0)){
 		return true;
 	}
+	
 	return false;
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeF) {
+  let degreeC = (degreeF -32) * (5/9);
+  if (!Number.isInteger(degreeC)){
+    return +degreeC.toFixed(1);
+  }
+  return degreeC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeC) {
+  let degreeF = (9/5 * degreeC) + 32;
+  if (!Number.isInteger(degreeF)){
+    return +degreeF.toFixed(1);
+  }
+  return degreeF;
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
