9979ddcfa513a4da6164e19ffaecc64bb3ed5aa3
xrishdog <108433356+xrishdog@users.noreply.github.com>
2023-08-09 14:10:01 -0400

    Finished first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+	let phrase = "";
 
+	if (num < 0){
+		return "ERROR";
+	}
+
+	for (let i = 0; i <num; i ++){
+		phrase += string;
+	}
+	return phrase;
 };
 
 // Do not edit below this line
