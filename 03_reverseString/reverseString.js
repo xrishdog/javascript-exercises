9f6698c3c915d1601ccc41eec265fbb8ae97db66
xrishdog <108433356+xrishdog@users.noreply.github.com>
2023-08-09 14:16:23 -0400

    Passed reverse String test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+	let stringArr = string.split("");
+	let newStr = "";
 
+	for (let i = stringArr.length -1; i >=0; i--){
+		newStr += stringArr[i];
+	}
+
+	return newStr;
 };
 
 // Do not edit below this line
