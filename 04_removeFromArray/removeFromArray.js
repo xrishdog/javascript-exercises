1958e547fab4ee802aa2f5f56a902036294703e3
xrishdog <108433356+xrishdog@users.noreply.github.com>
2023-08-09 14:38:58 -0400

    Learned to parse multiple arguments with arguments object. Completed remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
 const removeFromArray = function() {
+	const args = Array.from(arguments);
+
+	let mainArr = args[0];
+	let newArr = mainArr;
+
+
+	for (let i = 1; i < args.length ; i ++){
+
+		newArr = newArr.filter(item => item !== args[i]);
+		console.log(newArr);
+	}
+	return newArr
+
 
 };
 
